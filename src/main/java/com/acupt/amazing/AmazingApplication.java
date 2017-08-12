d89a34fc1b9c588c7e06eb0124f4eb48d4d96d78
liujie <liujie@taovip.com>
2017-08-12 23:25:30 +0800

    rename

--- src/main/java/com/acupt/amazing/AmazingApplication.java
+++ src/main/java/com/acupt/amazing/AmazingApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by liujie on 2017/8/12.
  */
 @SpringBootApplication
-public class AmazingController {
+public class AmazingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AmazingController.class, args);
+        SpringApplication.run(AmazingApplication.class, args);
     }
 }
